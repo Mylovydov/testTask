012b4c5c45dbbf4a21286d644d87d78db965ee55
Denys <den.milovidov.91@gmail.com>
2024-03-30 17:28:27 +0200

    change .env var name

--- src/services/api/api.ts
+++ src/services/api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.API_URL;
+const baseURL = process.env.RESOURCE_API_URL;
 
 const $apiInstance = axios.create({ baseURL });
 export default $apiInstance;
