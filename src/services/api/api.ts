e94cc959babcdbfff6bc18b981736264c4abe9d1
Denys <den.milovidov.91@gmail.com>
2024-03-31 12:55:03 +0300

    change baseURL

--- src/services/api/api.ts
+++ src/services/api/api.ts
@@ -2,5 +2,7 @@ import axios from 'axios';
 
 const baseURL = process.env.RESOURCE_API_URL;
 
-const $apiInstance = axios.create({ baseURL });
+const $apiInstance = axios.create({
+	baseURL: 'https://frontend-test-assignment-api.abz.agency/api/v1'
+});
 export default $apiInstance;
